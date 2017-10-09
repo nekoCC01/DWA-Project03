59ac6e99bc81e506bc592242381076cd43811980
Daniel Kasai <danielkasai2012@gmail.com>
2017-10-09 21:02:49 +0200

    Basic setup controllers - week6

---
+++ app/Http/Controllers/QuoteController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class QuoteController extends Controller
+{
+    public function index() {
+        return 'Show form to collect input from user.';
+    }
+
+    public function getQuotes() {
+        return 'Filter Quotes by user input und present them.';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,11 @@
 |
 */
 
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+
+Route::get('/', 'QuoteController@index');
+Route::get('/get-quotes','QuoteController@getQuotes');
