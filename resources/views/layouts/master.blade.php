ae8f303b7587faa82af632ae90b8d6852e89171e
Daniel Kasai <danielkasai2012@gmail.com>
2017-10-31 15:50:43 +0100

    improvement of pretender images, adding copyright Daniel Kasai

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -29,7 +29,7 @@
 
 <footer>
     <hr>
-    &copy; {{ date('Y') }}
+    &copy; {{ date('Y') }} Daniel Kasai
 </footer>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
